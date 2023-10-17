ac0e21879ab2f9252f2396247066d01c339295eb
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-10-17 19:22:56 +0700

    Panel grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,122 @@
-// wxWidgets "Hello world" Program
-// For compilers that support precompilation, includes "wx/wx.h".
-#include <wx/wxprec.h>
-#ifndef WX_PRECOMP
+// // wxWidgets "Hello world" Program
+// // For compilers that support precompilation, includes "wx/wx.h".
+// #include <wx/wxprec.h>
+// #ifndef WX_PRECOMP
+// #include <wx/wx.h>
+// #endif
+// class MyApp : public wxApp
+// {
+// public:
+//     virtual bool OnInit();
+// };
+// class MyFrame : public wxFrame
+// {
+// public:
+//     MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+// private:
+//     void OnHello(wxCommandEvent &event);
+//     void OnExit(wxCommandEvent &event);
+//     void OnAbout(wxCommandEvent &event);
+//     wxDECLARE_EVENT_TABLE();
+// };
+// enum
+// {
+//     ID_Hello = 1
+// };
+// wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+//     EVT_MENU(ID_Hello, MyFrame::OnHello)
+//         EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+//             EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+//                 wxEND_EVENT_TABLE()
+//                     wxIMPLEMENT_APP(MyApp);
+// bool MyApp::OnInit()
+// {
+//     MyFrame *frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
+//     frame->Show(true);
+//     return true;
+// }
+// MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+//     : wxFrame(NULL, wxID_ANY, title, pos, size)
+// {
+//     wxMenu *menuFile = new wxMenu;
+//     menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+//                      "Help string shown in status bar for this menu item");
+//     menuFile->AppendSeparator();
+//     menuFile->Append(wxID_EXIT);
+//     wxMenu *menuHelp = new wxMenu;
+//     menuHelp->Append(wxID_ABOUT);
+//     wxMenuBar *menuBar = new wxMenuBar;
+//     menuBar->Append(menuFile, "&File");
+//     menuBar->Append(menuHelp, "&Help");
+//     SetMenuBar(menuBar);
+//     CreateStatusBar();
+//     SetStatusText("Welcome to wxWidgets!");
+// }
+// void MyFrame::OnExit(wxCommandEvent &event)
+// {
+//     Close(true);
+// }
+// void MyFrame::OnAbout(wxCommandEvent &event)
+// {
+//     wxMessageBox("This is a wxWidgets' Hello world sample",
+//                  "About Hello World", wxOK | wxICON_INFORMATION);
+// }
+// void MyFrame::OnHello(wxCommandEvent &event)
+// {
+//     wxLogMessage("Hello world from wxWidgets!");
+// }
+
 #include <wx/wx.h>
-#endif
+
 class MyApp : public wxApp
 {
 public:
     virtual bool OnInit();
 };
+
+wxIMPLEMENT_APP(MyApp);
+
 class MyFrame : public wxFrame
 {
 public:
     MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+    ~MyFrame();
+};
 
-private:
-    void OnHello(wxCommandEvent &event);
-    void OnExit(wxCommandEvent &event);
-    void OnAbout(wxCommandEvent &event);
-    wxDECLARE_EVENT_TABLE();
-};
-enum
+MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+    : wxFrame(nullptr, wxID_ANY, title, pos, size)
 {
-    ID_Hello = 1
+    this->SetBackgroundColour(wxColor(0, 0, 0));
+
+    wxPanel *panel_top = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(300, 200));
+    panel_top->SetBackgroundColour(wxColor(100, 100, 200));
+
+    wxPanel *panel_bottom = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(150, 200));
+    panel_bottom->SetBackgroundColour(wxColor(100, 200, 100));
+
+    wxPanel *panel_bottom_right = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(150, 200));
+    panel_bottom_right->SetBackgroundColour(wxColor(200, 100, 100));
+
+    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer *sizer_bottom = new wxBoxSizer(wxHORIZONTAL);
+
+    sizer_bottom->Add(panel_bottom, 1, wxEXPAND | wxRIGHT, 10);
+    sizer_bottom->Add(panel_bottom_right, 0, wxEXPAND);
+
+    sizer->Add(panel_top, 1, wxEXPAND | wxTOP | wxLEFT | wxRIGHT, 10);
+    sizer->Add(sizer_bottom, 1, wxEXPAND | wxALL, 10);
+
+    this->SetSizerAndFit(sizer);
+}
+
+MyFrame::~MyFrame(){
+
 };
-wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-    EVT_MENU(ID_Hello, MyFrame::OnHello)
-        EVT_MENU(wxID_EXIT, MyFrame::OnExit)
-            EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
-                wxEND_EVENT_TABLE()
-                    wxIMPLEMENT_APP(MyApp);
+
 bool MyApp::OnInit()
 {
-    MyFrame *frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
-    frame->Show(true);
+    MyFrame *my_frame = new MyFrame("RemoteDesktop", wxDefaultPosition, wxDefaultSize);
+    my_frame->Show();
     return true;
-}
-MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
-    : wxFrame(NULL, wxID_ANY, title, pos, size)
-{
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
-    SetMenuBar(menuBar);
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
-}
-void MyFrame::OnExit(wxCommandEvent &event)
-{
-    Close(true);
-}
-void MyFrame::OnAbout(wxCommandEvent &event)
-{
-    wxMessageBox("This is a wxWidgets' Hello world sample",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
-}
-void MyFrame::OnHello(wxCommandEvent &event)
-{
-    wxLogMessage("Hello world from wxWidgets!");
 }
