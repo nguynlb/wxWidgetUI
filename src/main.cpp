8dc27f28af93f5d1e75e025202c0d208c8a48f3c
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-10-18 22:19:23 +0700

    linh tinh

--- src/main.cpp
+++ src/main.cpp
@@ -368,49 +368,165 @@
 
 // IMPLEMENT_APP(MyApp)
 
+// #include <wx/wx.h>
+// #include <wx/listctrl.h>
+// class MyApp : public wxApp
+// {
+// public:
+//     virtual bool OnInit();
+// };
+
+// wxIMPLEMENT_APP(MyApp);
+
+// class MyFrame : public wxFrame
+// {
+// public:
+//     MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+// };
+
+// MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+//     : wxFrame(nullptr, wxID_ANY, title, pos, size)
+// {
+//     wxListView *listView = new wxListView(this, wxID_ANY, wxDefaultPosition, wxSize(650, 450));
+//     listView->InsertColumn(0, "Name");
+//     listView->InsertItem(0, "Item");
+
+//     wxBoxSizer *s1 = new wxBoxSizer(wxVERTICAL);
+//     s1->Add(listView, 1, wxEXPAND | wxALL, 5);
+
+//     wxBoxSizer *s2 = new wxBoxSizer(wxHORIZONTAL);
+
+//     wxButton *cancel_btn = new wxButton(this, wxID_ANY, "Cancel");
+//     wxButton *submit_btn = new wxButton(this, wxID_ANY, "Submit");
+
+//     s2->Add(submit_btn, 0, wxRIGHT, 5);
+//     s2->Add(cancel_btn, 0);
+
+//     s1->Add(s2, 0, wxRIGHT | wxALIGN_RIGHT | wxBOTTOM, 5);
+
+//     this->SetBackgroundColour(wxColor(0, 0, 0));
+//     this->SetSizerAndFit(s1);
+// }
+
+// bool MyApp::OnInit()
+// {
+//     MyFrame *my_frame = new MyFrame("Remote Desktop App", wxDefaultPosition, wxDefaultSize);
+//     my_frame->Show();
+//     return true;
+// }
 #include <wx/wx.h>
-#include <wx/listctrl.h>
+#include <wx/filename.h>
+#include <wx/imaglist.h>
+#include <wx/sysopt.h>
+#include <wx/settings.h>
+#include <wx/artprov.h>
+
+// namespace ImageListExample
+// {
+//     class Frame : public wxFrame
+//     {
+//     public:
+//         Frame() : wxFrame(nullptr, wxID_ANY, "ImageList example")
+//         {
+//             wxInitAllImageHandlers();
+//             SetClientSize(wxSize(300, 250));
+
+//             panel->Bind(wxEVT_LEFT_DOWN, [&](wxMouseEvent &event)
+//                         { LoadImages(); });
+
+//             picture->SetSize(150, 150);
+//             picture->SetBackgroundColour(wxSystemSettings::GetColour(wxSystemColour::wxSYS_COLOUR_WINDOW));
+//             picture->Bind(wxEVT_LEFT_DOWN, [&](wxMouseEvent &event)
+//                           { LoadImages(); });
+
+//             buttonUp->Enable(false);
+//             buttonUp->Bind(wxEVT_BUTTON, [&](wxCommandEvent &event)
+//                            {
+//         if (currentImageIndex > 0) picture->SetBitmap(pictures->GetBitmap(--currentImageIndex));
+//         picture->SetSize(150, 150);
+//         buttonUp->Enable(currentImageIndex > 0);
+//         buttonDown->Enable(currentImageIndex < pictures->GetImageCount() - 1); });
+
+//             buttonDown->Enable(false);
+//             buttonDown->Bind(wxEVT_BUTTON, [&](wxCommandEvent &event)
+//                              {
+//         if (currentImageIndex < pictures->GetImageCount()) picture->SetBitmap(pictures->GetBitmap(++currentImageIndex));
+//         picture->SetSize(150, 150);
+//         buttonUp->Enable(currentImageIndex > 0);
+//         buttonDown->Enable(currentImageIndex < pictures->GetImageCount() - 1); });
+
+//             Show();
+//             LoadImages();
+//         }
+
+//     private:
+//         void LoadImages()
+//         {
+//             wxFileDialog openFileDialog(this, wxEmptyString, wxEmptyString, wxEmptyString, "All Image Files|*.bmp;*.gif;*.jpg;*.jpeg;*.png;*.tif;*.tiff;*.xpm|Bitmap Files|*.bmp|Gif Files|*.gif|Jpeg Files|*.jpg;*.jpeg|Png Files|*.png|Tiff Files|*.tif;*.tiff|xpm Files|*.xpm", wxFD_OPEN | wxFD_FILE_MUST_EXIST | wxFD_MULTIPLE);
+//             if (openFileDialog.ShowModal() == wxID_OK)
+//             {
+//                 wxArrayString filenames;
+//                 openFileDialog.GetFilenames(filenames);
+//                 pictures->RemoveAll();
+//                 for (auto filename : filenames)
+//                 {
+//                     auto image = wxImage(openFileDialog.GetDirectory() + wxFileName::GetPathSeparator() + filename);
+//                     pictures->Add(wxBitmap(image.Rescale(128, 128, wxIMAGE_QUALITY_HIGH)));
+//                 }
+//                 currentImageIndex = 0;
+//                 picture->SetBitmap(pictures->GetBitmap(currentImageIndex));
+//                 picture->SetSize(150, 150);
+//                 buttonUp->Enable(currentImageIndex > 0);
+//                 buttonDown->Enable(currentImageIndex < pictures->GetImageCount() - 1);
+//             }
+//         }
+
+//         int currentImageIndex = 0;
+//         wxPanel *panel = new wxPanel(this);
+//         wxImageList *pictures = new wxImageList(128, 128);
+//         wxStaticBitmap *picture = new wxStaticBitmap(panel, wxID_ANY, wxNullBitmap, {75, 25}, wxDefaultSize, wxBORDER_SUNKEN);
+//         wxBitmapButton *buttonUp = new wxBitmapButton(panel, wxID_UP, wxArtProvider::GetBitmap(wxART_GO_UP, wxART_BUTTON), {75, 200}, {75, 25});
+//         wxBitmapButton *buttonDown = new wxBitmapButton(panel, wxID_DOWN, wxArtProvider::GetBitmap(wxART_GO_DOWN, wxART_BUTTON), {150, 200}, {75, 25});
+//     };
+
+//     class Application : public wxApp
+//     {
+//         bool OnInit() override
+//         {
+//             wxSystemOptions::SetOption("osx.openfiledialog.always-show-types", 1);
+//             new Frame();
+//             return true;
+//         }
+//     };
+// }
+
+// wxIMPLEMENT_APP(ImageListExample::Application);
+
 class MyApp : public wxApp
 {
 public:
     virtual bool OnInit();
 };
 
-wxIMPLEMENT_APP(MyApp);
-
 class MyFrame : public wxFrame
 {
 public:
-    MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+    MyFrame() : wxFrame(nullptr, wxID_ANY, "Remote Desktop", wxDefaultPosition, wxDefaultSize)
+    {
+        wxImage pic = wxImage("./asserts/Z_08.jpg");
+        image_list->Add(wxBitmap(pic.Rescale(128, 128, wxIMAGE_QUALITY_HIGH)));
+    }
+
+private:
+    wxPanel *panel = new wxPanel(this);
+    wxImageList *image_list = new wxImageList(256, 256);
+    wxStaticBitmap *pictures = new wxStaticBitmap(this, wxID_ANY, wxNullBitmap);
 };
 
-MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
-    : wxFrame(nullptr, wxID_ANY, title, pos, size)
-{
-    wxListView *listView = new wxListView(this, wxID_ANY, wxDefaultPosition, wxSize(650, 450));
-    listView->InsertColumn(0, "Name");
-    listView->InsertItem(0, "Item");
-
-    wxBoxSizer *s1 = new wxBoxSizer(wxVERTICAL);
-    s1->Add(listView, 1, wxEXPAND | wxALL, 5);
-
-    wxBoxSizer *s2 = new wxBoxSizer(wxHORIZONTAL);
-
-    wxButton *cancel_btn = new wxButton(this, wxID_ANY, "Cancel");
-    wxButton *submit_btn = new wxButton(this, wxID_ANY, "Submit");
-
-    s2->Add(submit_btn, 0, wxRIGHT, 5);
-    s2->Add(cancel_btn, 0);
-
-    s1->Add(s2, 0, wxRIGHT | wxALIGN_RIGHT | wxBOTTOM, 5);
-
-    this->SetBackgroundColour(wxColor(0, 0, 0));
-    this->SetSizerAndFit(s1);
-}
+wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit()
 {
-    MyFrame *my_frame = new MyFrame("Remote Desktop App", wxDefaultPosition, wxDefaultSize);
-    my_frame->Show();
+    (new MyFrame)->Show();
     return true;
 }
